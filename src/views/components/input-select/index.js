4f584c46728530602d938bd1bf9ade34b53ba883
cxyi <cxyi@servyou.com.cn>
2020-02-25 20:30:32 +0800

    完成输入下拉框

--- src/views/components/input-select/index.js
+++ src/views/components/input-select/index.js
@@ -4,16 +4,41 @@ import React from 'react';
 export default class InputSelect extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            value: props.value
+        };
+        this.list = [
+            { key: '1', value: 'cat' },
+            { key: '2', value: 'dog' },
+            { key: '3', value: 'monkey' }
+        ];
+    }
+    componentWillReceiveProps(nextProps) {
+        this.setState({ value: nextProps.value });
+    }
+    selectChange(e) {//select的change事件
+        let value = e.target.value, item = null;
+        for (let i = 0; i < this.list.length; i++) {
+            if (this.list[i].key === value) {
+                item = this.list[i];
+                break;
+            }
+        }
+        this.setState({ value: item.value });
+        this.props.onChange(item.value);
+    }
+    inputChange(e) {//input的change事件
+        let value = e.target.value;
+        this.setState({ value });
+        this.props.onChange(value);
     }
     render() {
-        return <div className="inputSelect" style={{ width: this.props.width || '100%' }}>
-            <select className="is is-select">
-                <option value="1">cat</option>
-                <option value="2">dog</option>
-                <option value="3">monkey</option>
+        return <div className="inputSelect" style={{ width: this.props.style.width || '100%' }}>
+            <select className="is is-select" onChange={(e) => this.selectChange(e)}>
+                {this.list.map((item) => <option key={item.key} value={item.key}>{item.value}</option>)}
             </select>
-            <input className="is is-input" style={{ width: this.props.width - 20 }} />
+            <input value={this.state.value} onChange={(e) => this.inputChange(e)}
+                className="is is-input" style={{ width: this.props.style.width - 20 }} />
         </div>
     }
 }
--- src/views/demo/inputSelect.js
+++ src/views/demo/inputSelect.js
@@ -4,11 +4,16 @@ import InputSelect from '../components/input-select';
 export default class InputSelectDemo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            value: '123'
+        };
     }
     render() {
-        return <div>
-            <InputSelect width={200} />
+        let { value } = this.state;
+        return <div style={{ marginLeft: '20px', marginTop: '20px' }}>
+            <InputSelect value={value} onChange={(value) => this.setState({ value })}
+                style={{ width: 200 }} />
+            <p style={{ marginTop: 100 }}>表单项值为：{value}</p>
         </div>
     }
 }
